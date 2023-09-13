4da2127c2d59ee442ac9500faa76e14d20aafc96
gilles <yembitrenaud@gmail.com>
2023-09-13 22:12:54 +0200

    tasks

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,18 @@ Route::get('/', function () {
 
 Route::prefix('admin')->group(function () {
 
-    Route::match(['get', 'post'], '/gestion', 'App\Http\Controllers\AuthController@index');
+    Route::match(['get', 'post'], '/', 'App\Http\Controllers\AuthController@index');
 
     Route::match(['get', 'post'], '/login', 'App\Http\Controllers\AuthController@login');
 
     Route::group(['middleware' => ['admin']], function () {
 
-        Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index');
-        Route::get('/logout', 'App\Http\Controllers\DashboardController@logout'); 
+        Route::match(['get', 'post'], '/tasks', 'App\Http\Controllers\TaskController@details');
+Route::match(['get', 'post'], '/tasks/store', 'App\Http\Controllers\TaskController@store');
+Route::match(['get', 'post'], '/tasks/edit/{id}', 'App\Http\Controllers\TaskController@edit');
+Route::match(['get', 'post'], '/tasks/delete/{id}', 'App\Http\Controllers\TaskController@delete');
 
-        Route::match(['get', 'post'], '/tasks', 'App\Http\Controllers\EvenementController@details');
-        Route::match(['get', 'post'], '/tasks/store', 'App\Http\Controllers\EvenementController@store');
-        Route::match(['get', 'post'], '/tasks/edit/{id}', 'App\Http\Controllers\EvenementController@edit');
-        Route::match(['get', 'post'], '/tasks/delete/{id}', 'App\Http\Controllers\EvenementController@delete');
+       
 
     });
 });
